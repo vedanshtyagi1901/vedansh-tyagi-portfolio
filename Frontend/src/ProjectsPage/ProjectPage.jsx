ef8437ec4c9b3d2976b47bd2005c214614c3262e
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-06-24 19:08:59 +0530

    footer at bottom

--- Frontend/src/ProjectsPage/ProjectPage.jsx
+++ Frontend/src/ProjectsPage/ProjectPage.jsx
@@ -5,11 +5,17 @@ import Footer from "../HomePage/Components/Footer"
 
 function ProjectPage() {
   return (
-    <div>
-        <Navbar/>
-        <div className='h-20'></div>
-        <ProjectsGrid/>
-        <Footer/>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="h-20"></div>
+      
+      {/* Main content with grow to push footer down */}
+      <div className="flex-grow">
+        <ProjectsGrid />
+      </div>
+
+      {/* Footer stays at the bottom */}
+      <Footer />
     </div>
   )
 }
